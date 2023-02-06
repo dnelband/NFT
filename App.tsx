93db3aeeb155d4045467a0d41b3ef281ceb23abb
david nelband <torun.wikstrom@gmail.com>
2023-02-06 12:42:02 +0100

    First Commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My NFT app!</Text>
+      <Text>My NFT app ok!</Text>
       <StatusBar style="auto" />
     </View>
   );
